c6d2d3808db73d6f5da247ed286f1eff8bd34e00
Hilton Shumway <hillshum@gmail.com>
2025-02-25 22:40:41 -0600

    WIP on custom snapshot crops

--- octoprint_InfoOrbs/__init__.py
+++ octoprint_InfoOrbs/__init__.py
@@ -87,13 +87,18 @@ class InfoorbsPlugin(
         # get image size
         height, width = img.shape[:2]
 
+        corners = ((2, 3), (900, 300))
+
+        # crop image at the corners
+        img = img[corners[0][0] : corners[1][0], corners[0][1] : corners[1][1]]
+
         # crop image to square
-        if height > width:
-            offset = (height - width) // 2
-            img = img[offset : offset + width, 0:width]
-        else:
-            offset = (width - height) // 2
-            img = img[0:height, offset : offset + height]
+        # if height > width:
+        #     offset = (height - width) // 2
+        #     img = img[offset : offset + width, 0:width]
+        # else:
+        #     offset = (width - height) // 2
+        #     img = img[0:height, offset : offset + height]
 
         # scale image to 240x240
         img = cv2.resize(img, (240, 240), interpolation=cv2.INTER_AREA)
